b78d5093f15e2cfe9d9e8c5b14b875bf2b14623f
Joanna von Berg <Joanna von Berg>
2017-02-01 16:52:30 +0100

    Added methylation data for BRCA, and wrote a function to write the new omics dataframe with survival data to a flat text file

--- RTCGA_test.R
+++ RTCGA_test.R
@@ -6,10 +6,12 @@
 # biocLite("RTCGA.clinical")
 # biocLite("RTCGA.mRNA")
 # biocLite("RTCGA.miRNASeq")
+# biocLite("RTCGA.methylation")
 # library("RTCGA")
 # library("RTCGA.clinical")
 # library("RTCGA.mRNA")
 # library("RTCGA.miRNASeq")
+# library("RTCGA.methylation")
 
 AddSurvivalData <- function(omics, clin){
   patients_omics <- substr(rownames(omics), 1, 12)
@@ -36,6 +38,10 @@ AddSurvivalData <- function(omics, clin){
   return(omics)
 }
 
+writeFile <- function(df, fn){
+  write.table(df, sprintf("%s_with_survival_data.txt", fn))
+}
+
 # preparing clinical data
 clin <- BRCA.clinical
 rownames(clin) <- toupper(clin$patient.bcr_patient_barcode)
@@ -49,10 +55,18 @@ patients_clin <- rownames(clin)
 mRNA <- BRCA.mRNA
 rownames(mRNA) <- mRNA$bcr_patient_barcode
 mRNA2 <- AddSurvivalData(mRNA, clin)
+writeFile(mRNA2, "mRNA2")
 
 # miRNA-seq
 miRNA <- BRCA.miRNASeq
 miRNA2 <- AddSurvivalData(miRNA, clin)
+writeFile(miRNA2, "miRNA2")
+
+# methylation
+methyl <- BRCA.methylation
+rownames(methyl) <- toupper(methyl$bcr_patient_barcode)
+methyl2 <- AddSurvivalData(methyl, clin)
+writeFile(methyl2, "methylation")
 
 # discretize days_to_death in early and late death
 # tran3$days_to_death_discrete <- cut(tran3$days_to_death, breaks = 2, labels = c("early", "late"))
