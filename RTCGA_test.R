c9552ea417a9d017e5a6ccea18cd1662b67fbf4f
Joanna von Berg <Joanna von Berg>
2017-02-01 15:36:51 +0100

    Written function to add clinical data to any omics data source

--- RTCGA_test.R
+++ RTCGA_test.R
@@ -2,12 +2,14 @@
 # https://www.r-bloggers.com/rtcga-factory-of-r-packages-quick-guide/
 
 source("https://bioconductor.org/biocLite.R")
-#biocLite("RTCGA")
+biocLite("RTCGA")
 biocLite("RTCGA.clinical")
 biocLite("RTCGA.mRNA")
+biocLite("RTCGA.miRNASeq")
 library("RTCGA")
 library("RTCGA.clinical")
 library("RTCGA.mRNA")
+library("RTCGA.miRNASeq")
 library("ggplot2")
 library("survminer")
 
@@ -41,32 +43,42 @@ rownames(tran2) <- toupper(tran2$bcr_patient_barcode)
 # # merge_patients <- patients_tran %in% patients_clin
 
 patients_clin <- rownames(clin2)
+# because there can be several samples per patient, and only the first 12 characters identify the patient:
 patients_tran <- substr(rownames(tran2), 1, 12)
 tran2$patient_id <- patients_tran
+# I want only the survival data for patients that are also in the transcriptomic dataset:
 merge_patients <- patients_clin %in% patients_tran
-#merge_patients <- patients_tran %in% patients_clin
-
-# keep only the patients with clinical data
-nrow(tran2)
-# 590
-sum(merge_patients)
-# 527
-# there's a difference, because there are more samples than patients (multiple samples are possible per patient)
 days_to_death <- clin2$patient.days_to_death[merge_patients]
-names(days_to_death) <- rownames(clin2)[merge_patients]
+# names(days_to_death) <- rownames(clin2)[merge_patients]
 
 # add the days to death for each transcriptomic sample
 tran2$days_to_death <- rep(-1, nrow(tran2))
+tran2$days_to_last_followup <- rep(-1, nrow(tran2))
 for (patient in patients_clin){
   tran2$days_to_death[tran2$patient_id == patient] <- days_to_death[patient]
+  tran2$days_to_last_followup[tran2$patient_id == patient]
 }
-
-# remove all samples for which days_to_death == NA (better to do this before, if it is at all sensible to do)
-tran3 <- tran2[!is.na(tran2$days_to_death),]
 
 # discretize days_to_death in early and late death
 tran3$days_to_death_discrete <- cut(tran3$days_to_death, breaks = 2, labels = c("early", "late"))
 
-# integrate gene lists based on pathways to perform feature selection
+# miRNA-seq
+miRNA <- BRCA.miRNASeq
+clin <- BRCA.clinical
+rownames(clin) <- toupper(clin$patient.bcr_patient_barcode)
+patients_clin <- rownames(clin)
 
-# download pathway gene lists
+AddSurvivalData <- function(omics, clin){
+  patients_omics <- substr(rownames(omics), 1, 12)
+  omics$patient_ID <- patients_omics
+  merge_patients <- patients_clin %in% patients_omics
+  days_to_death <- clin$patient.days_to_death[merge_patients]
+  days_to_last_followup <- clin$patient.days_to_last_followup[merge_patients]
+  
+  omics$days_to_death <- rep(-1, nrow(omics))
+  omics$days_to_last_followup <- rep(-1, nrow(omics))
+  for (patient in merge_patients){
+    omics$days_to_death[omics$patient_id == patient] <- days_to_death[patient]
+    omics$days_to_last_followup[omics$patient_id == patient] <- days_to_last_followup[patient]
+  }
+}
