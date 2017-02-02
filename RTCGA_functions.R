4e215ccb561808af72766b68c2224b337ea24aee
Joanna von Berg <Joanna von Berg>
2017-02-02 16:12:38 +0100

    Changed a few things, so a dataframe and a file are created for each disease-omic combination

--- RTCGA_functions.R
+++ RTCGA_functions.R
@@ -1,23 +1,39 @@
 # This function wants as input:
-# a string that indicates the disease (look up specific string)
+# a vector of strings that indicates the which diseases should be considered (look up specific string)
 # a vector of strings that indicate which omic source should be considered (look up specific string)
 # a Boolean that indicates whether .txt files should be written for each omic source 
 #
-# and returns nothing, but it creates a data-frame with survival data for each omic data source
-AddSurvDataToOmics <- function(disease, omics, save = FALSE){
-  # preparing clinical data
-  if(!exists("clin")){
-    clin <- get(sprintf("%s.%s", disease, "clinical"))
-    rownames(clin) <- toupper(clin$patient.bcr_patient_barcode)
-    patients_clin <- rownames(clin)
-  }
-  for(om in omics){
-    assign(om, get(sprintf("%s.%s", disease, om)))
-    df <- get(om)
-    rownames(df) <- toupper(df$bcr_patient_barcode)
-    df <- AddSurvivalData(df, clin)
-    if(save){
-      writeFile(df, om)
+# and returns nothing, but creates a data-frame with survival data for each disease and omic data source combination
+AddSurvDataToOmics <- function(diseases, omics, save = FALSE){
+  for(dis in diseases){
+    # preparing clinical data
+    if(!exists("clin")){#}, where = .GlobalEnv)){
+      clin <<- get(sprintf("%s.%s", dis, "clinical"))
+      patients_clin <<- toupper(clin$patient.bcr_patient_barcode)
+      rownames(clin) <<- patients_clin
+    }
+    for(om in omics){
+      string <- sprintf("%s.%s", dis, om)
+      if(exists(string)){
+        df <- get(string)
+      }
+      else{
+        next
+      }
+      print(sprintf("dimensions of df for %s: %s", om, dim(df)))
+      dis_om <- sprintf("%s_%s", dis, om)
+      if(om == "CNV"){
+        assign(dis_om, AddSurvivalData(df, clin, om), envir = .GlobalEnv)
+      }
+      else{
+        if(rownames(df)[1] == 1){
+          rownames(df) <- toupper(df$bcr_patient_barcode)
+        }
+        assign(dis_om, AddSurvivalData(df, clin, om), envir = .GlobalEnv)
+      }
+      if(save){
+        writeFile(df, dis_om)
+      }
     }
   }
 }
@@ -29,24 +45,24 @@ AddSurvDataToOmics <- function(disease, omics, save = FALSE){
 # and returns:
 # - a dataframe like the original omic input, with extra columns patient_id, days_to_death and days_to_last_followup
 # NB. It is not specific to any disease
-AddSurvivalData <- function(omics, clin){
-  patients_omics <- substr(rownames(omics), 1, 12)
+AddSurvivalData <- function(omics, clin, om){
+  if(om == "CNV"){
+    patients_omics <- substr(omics$Sample, 1, 12)
+  }
+  else{
+    patients_omics <- substr(rownames(omics), 1, 12)
+  }
   omics$patient_id <- patients_omics
-  merge_patients <- patients_clin %in% patients_omics
+  merge_patients <- rownames(clin) %in% patients_omics
   
-  patients <- patients_clin[merge_patients]
-  
+  patients <- rownames(clin)[merge_patients]
   days_to_death <- clin$patient.days_to_death[merge_patients]
   names(days_to_death) <- patients
-  #pat <- patients[1]
-  #print(days_to_death[pat])
   days_to_last_followup <- clin$patient.days_to_last_followup[merge_patients]
   names(days_to_last_followup) <- patients
-  #print(days_to_last_followup)
-  
+
   omics$days_to_death <- rep(-1, nrow(omics))
   omics$days_to_last_followup <- rep(-1, nrow(omics))
-  
   for (patient in patients){
     omics$days_to_death[omics$patient_id == patient] <- days_to_death[patient]
     omics$days_to_last_followup[omics$patient_id == patient] <- days_to_last_followup[patient]
