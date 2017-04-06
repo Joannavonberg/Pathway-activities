c894d0e6c580bdb8a6356499f1a72c2fbe8e111e
berg0 <berg0@sgbit-02.bit.uni-bonn.de>
2017-04-06 10:34:03 +0200

    Added try-statements

--- load_data.R
+++ load_data.R
@@ -1,10 +1,14 @@
-diseases <- c("BRCA", "GBM", "OV")
-omics <- c("mRNA", "CNV", "miRNASeq", "methylation")
+diseases <- c("BRCA", "GBM", "OV", "PRAD")
+omics <- c("mRNA", "mRNASeq", "CNV", "miRNA", "miRNASeq", "methylation")
 
 for(d in diseases){
   load(sprintf("/home/abidata/Joanna/%s/%s_pheno.rda", d, d))
   for(o in omics){
-    load(sprintf("/home/abidata/Joanna/%s/%s/%s_%s.rda", d, o, d, o))
-    load(sprintf("/home/abidata/Joanna/%s/%s/%s_%s_patients.rda", d, o, d, o))
+    try(
+      load(sprintf("/home/abidata/Joanna/%s/%s/%s_%s.rda", d, o, d, o))
+    )
+    try(
+      load(sprintf("/home/abidata/Joanna/%s/%s/%s_%s_patients.rda", d, o, d, o))
+    )
   }
 }
